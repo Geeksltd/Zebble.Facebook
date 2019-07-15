d4fc3acb00ec190be7a1c30ac67f8168431b7d2d
Paymon <paymon@geeks.ltd.uk>
2019-07-15 09:05:13 +0100

    no message

--- Android/Facebook/Facebook.AccessToken.cs
+++ Android/Facebook/Facebook.AccessToken.cs
@@ -6,6 +6,17 @@
     internal static class FacebookExtensions
     {
         static DateTime UnixStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        public static DateTime ToDateTime(this Date date) => UnixStart.AddMilliseconds(date.Time);
+        public static DateTime ToDateTime(this Date date)
+        {
+            try
+            {
+                return UnixStart.AddMilliseconds(date.Time);
+            }
+            catch (Exception ex)
+            {
+                Device.Log.Error(ex);
+                return DateTime.Now.AddMinutes(5);
+            }
+        }
     }
 }
