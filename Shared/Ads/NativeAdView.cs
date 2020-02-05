396311098ffec2bc868c940fcb2f8622892437f4
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-02-05 21:47:27 +0330

    Add FacebookAds

---
+++ Shared/Ads/NativeAdView.cs
@@ -0,0 +1,20 @@
+﻿namespace Zebble.FacebookAds
+{
+    public class NativeAdView : View, IRenderedBy<FacebookAdsViewRenderer>
+    {
+        public readonly Bindable<NativeAdInfo> Ad = new Bindable<NativeAdInfo>(new NativeAdInfo());
+        internal readonly AsyncEvent RotateRequested = new AsyncEvent();
+
+        public void Rotate() => RotateRequested.RaiseOn(Thread.UI);
+
+        public TextView HeadLineView { get; set; }
+        public TextView BodyView { get; set; }
+        public Button CallToActionView { get; set; }
+        public ImageView IconView { get; set; }
+        public TextView SocialContextView { get; set; }
+        public TextView AdvertiserView { get; set; }
+        public NativeAdMediaView MediaView { get; set; }
+
+        public AdAgent Agent { get; set; }
+    }
+}
---
+++ iOS/Ads/FacebookAdsViewRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace Zebble.FacebookAds
+{
+    public class FacebookAdsViewRenderer : INativeRenderer
+    {
+        UIView Result;
+
+        public Task<UIView> Render(Renderer renderer)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Dispose()
+        {
+            Result.Dispose();
+            Result = null;
+        }
+    }
+}
